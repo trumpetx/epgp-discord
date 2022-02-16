d36a1dbfa5343bafb9dede36dda0f01a28a6b3ba
David Greene <david@trumpetx.com>
2022-02-15 21:12:39 -0600

    Disabling JUST the content security policy a different way

--- web/web.js
+++ web/web.js
@@ -19,11 +19,21 @@ const moment = require('moment');
 const _ = require('lodash');
 const discord = require('../discord');
 
-app.use(
-  helmet({
-    contentSecurityPolicy: false
-  })
-);
+// app.use(helmet.contentSecurityPolicy());
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded());
